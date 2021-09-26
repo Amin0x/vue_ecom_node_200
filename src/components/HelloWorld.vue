499ae9f54bad4d96a272e1a9fa3700e4c7f4fd74
amin <garogigi@gmail.com>
2021-09-27 00:21:53 +0300

    CD CI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,76 @@
 <template>
   <div class="hello">
-      <div class="aa-search">
-        <div class="search-title">
-          Search and Compare
-        </div>
-        <div class="aa-search-wrap">
-          <input type="text">
-          <button>Search</button>
-        </div>
+    <div class="aa-search">
+      <div class="search-title">Search and Compare</div>
+      <div class="aa-search-wrap">
+        <input type="text">
+        <button>Search</button>
       </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+ 
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  components: {
+    
+  },
+  computed: {
+    
+  },
+  mounted() {
+    
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App",
+     
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .aa-search{
-    padding: 120px 0;
-    background-image: url('../assets/homepage33.png');
-  }
+.aa-search {
+  padding: 120px 0;
+  background-image: url("../assets/homepage33.png");
+}
 
-  .aa-search .search-title{
-    
-      color: #ffffff;
-      font-size: 40px;
-      font-weight: 700;
-  }
+.aa-search .search-title {
+  color: #ffffff;
+  font-size: 40px;
+  font-weight: 700;
+}
 
-  .aa-search input{
-        border-width: 1px;
-    height: 52px;
-    padding: 0 130px 0 20px;
-        transition: all 0.5s ease-out;
-    background: #f6f6f6;
-    border: 3px solid #ececec;
-    width: 100%;
-    outline: none;
-  }
-  .aa-search .aa-search-wrap{
-    position: relative;
-        max-width: 600px;
-        margin: 30px auto;
-  }
-  .aa-search-wrap button{
-    padding: 0 35px;
-    height: 100%;
-    right: 0;
-    top: 0;
-    font-size: 100%!important;
-    color: #fff!important;
-    background-color: #ff4136;
-    border: 1px solid #ff4136;
-    position: absolute;
-   
-  }
+.aa-search input {
+  border-width: 1px;
+  height: 52px;
+  padding: 0 130px 0 20px;
+  transition: all 0.5s ease-out;
+  background: #f6f6f6;
+  border: 3px solid #ececec;
+  width: 100%;
+  outline: none;
+}
+.aa-search .aa-search-wrap {
+  position: relative;
+  max-width: 600px;
+  margin: 30px auto;
+}
+.aa-search-wrap button {
+  padding: 0 35px;
+  height: 100%;
+  right: 0;
+  top: 0;
+  font-size: 100% !important;
+  color: #fff !important;
+  background-color: #ff4136;
+  border: 1px solid #ff4136;
+  position: absolute;
+}
 </style>
--- src/components/ProdCardListComponent.vue
+++ src/components/ProdCardListComponent.vue
@@ -3,10 +3,21 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">
+                        <img src="" alt="">
+                    </div>
 
                     <div class="card-body">
-                        I'm an example component.
+                       <div class="d-flex align-items-center justify-content-between">
+                           <div class="price"></div>
+                           <div class="brand"></div>
+                       </div>
+                       <div class="title"></div>
+                       <div class="buttons">
+                           <div class="aabutton"><button type="button" class="btn" role="button"></button></div>
+                           <div class="aabutton"><button type="button" class="btn" role="button"></button></div>
+                           <div class="aabutton"><button type="button" class="btn" role="button"></button></div>
+                       </div>
                     </div>
                 </div>
             </div>
